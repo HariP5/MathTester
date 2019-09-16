22614c44ebdb75fd991440124cb73d8e6bde53bc
Hari Pokhrel <hari.pokhrel.2020@student.lhschools.org>
2019-09-16 09:45:47 -0400

    Added the last line of the code

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -19,8 +19,15 @@ public class Main {
 	System.out.println(firstWord+(firstNumber+secondNumber));
 	System.out.println("firstNumber + secondNumber is " +(firstNumber+secondNumber));
 	System.out.println("firstNumber + secondNumber is " +(firstNumber-secondNumber));
+	System.out.println("secondNumber / firstNumber is " +(secondNumber/firstNumber));
+	System.out.println("firstNumber * secondNumber is " +(firstNumber*secondNumber));
+	System.out.println("firstNumber + secondNumber is " +(firstNumber-secondNumber));
+	System.out.println("The modulus of the secondNumber by the firstNumber"+(firstNumber%secondNumber));
 
 
 
-    }
+
+
+
+	}
 }
